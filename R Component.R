5dd90cb65c24c2b293f24de307343f8f9a4879a4
Derpilicious <111034596+Derpilicious@users.noreply.github.com>
2023-07-21 19:13:27 -0400

    sql query

--- R Component.R
+++ R Component.R
@@ -24,3 +24,14 @@ Sys.sleep(10)
 ws$send("{\"type\": \"subscribe\",\"channels\": [\"level2\"],\"product_ids\": [\"BTC-USD\"]}")
 Sys.sleep(3)
 ws$close()
+
+sqldf('SELECT
+      JSON_VALUE(json_data, \'$.type\') AS type,
+      JSON_VALUE(json_data, \'$.product_id\') AS product_id,
+      JSON_VALUE(json_data, \'$.changes[0][0]\') AS change_type,
+      JSON_VALUE(json_data, \'$.changes[0][1]\') AS change_value,
+      JSON_VALUE(json_data, \'$.changes[0][2]\') AS change_amount,
+      JSON_VALUE(json_data, \'$.time\') AS time
+      FROM
+      data_table;'
+)
