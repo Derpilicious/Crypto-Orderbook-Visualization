4e9f1e742014d5b7312d306ab5b2ab42b1a51950
Derpilicious <111034596+Derpilicious@users.noreply.github.com>
2023-07-22 02:14:25 -0400

    loading information into db

--- R Component.R
+++ R Component.R
@@ -7,8 +7,19 @@ library(websocket)
 ws <- websocket::WebSocket$new('wss://ws-feed.exchange.coinbase.com',autoConnect = FALSE)
 
 ws$onMessage(function(message) {
-  d <- message$data
-  print(message$data)
+  #print(message$data)
+  parsed_json <- fromJSON(json_string)
+  
+  # Create separate columns for each key-value pair
+  type <- parsed_json$type
+  product_id <- parsed_json$product_id
+  change_type <- parsed_json$changes[[1]][1]
+  price <- as.numeric(parsed_json$changes[[1]][2])
+  amount <- as.numeric(parsed_json$changes[[1]][3])
+  time <- parsed_json$time
+  
+  # Combine the extracted data into a data frame
+  result_df <- data.frame(type, product_id, change_type, price, amount, time)
 })
 
 ws$onClose(function(event) {
@@ -25,13 +36,19 @@ ws$send("{\"type\": \"subscribe\",\"channels\": [\"level2\"],\"product_ids\": [\
 Sys.sleep(3)
 ws$close()
 
-sqldf('SELECT
-      JSON_VALUE(json_data, \'$.type\') AS type,
-      JSON_VALUE(json_data, \'$.product_id\') AS product_id,
-      JSON_VALUE(json_data, \'$.changes[0][0]\') AS change_type,
-      JSON_VALUE(json_data, \'$.changes[0][1]\') AS change_value,
-      JSON_VALUE(json_data, \'$.changes[0][2]\') AS change_amount,
-      JSON_VALUE(json_data, \'$.time\') AS time
-      FROM
-      data_table;'
-)
+
+
+# Print the result
+print(result_df)
+
+
+#sqldf('SELECT
+#      JSON_VALUE(json_data, \'$.type\') AS type,
+#      JSON_VALUE(json_data, \'$.product_id\') AS product_id,
+#      JSON_VALUE(json_data, \'$.changes[0][0]\') AS change_type,
+#      JSON_VALUE(json_data, \'$.changes[0][1]\') AS change_value,
+#      JSON_VALUE(json_data, \'$.changes[0][2]\') AS change_amount,
+#      JSON_VALUE(json_data, \'$.time\') AS time
+#      FROM
+#      data_table;'
+#)
