19ac92e44a16a6d74de60f64f5b606e83a45594b
Roy Luo <luor19@mcmaster.ca>
2023-07-23 22:55:21 -0500

    print tout relevatn informaiton

--- R Component.R
+++ R Component.R
@@ -1,25 +1,22 @@
 # importing packages/libraries
 #library(httr)
-library(jsonlite)
 #library(glue)
+library(jsonlite)
 library(websocket)
 
 ws <- websocket::WebSocket$new('wss://ws-feed.exchange.coinbase.com',autoConnect = FALSE)
 
 ws$onMessage(function(message) {
   #print(message$data)
-  parsed_json <- fromJSON(json_string)
-  
+  parsed_json <- fromJSON(message$data)
   # Create separate columns for each key-value pair
   type <- parsed_json$type
   product_id <- parsed_json$product_id
-  change_type <- parsed_json$changes[[1]][1]
-  price <- as.numeric(parsed_json$changes[[1]][2])
-  amount <- as.numeric(parsed_json$changes[[1]][3])
+  change_type <- parsed_json$changes[1]
+  price <- parsed_json$changes[2]
+  amount <- parsed_json$changes[3]
   time <- parsed_json$time
-  
-  # Combine the extracted data into a data frame
-  result_df <- data.frame(type, product_id, change_type, price, amount, time)
+  print(paste(product_id,":",change_type,price))
 })
 
 ws$onClose(function(event) {
@@ -31,16 +28,10 @@ ws$onOpen(function(event) {
 })
 
 ws$connect()
-Sys.sleep(10)
+Sys.sleep(5)
 ws$send("{\"type\": \"subscribe\",\"channels\": [\"level2\"],\"product_ids\": [\"BTC-USD\"]}")
-Sys.sleep(3)
+Sys.sleep(1)
 ws$close()
-
-
-
-# Print the result
-print(result_df)
-
 
 #sqldf('SELECT
 #      JSON_VALUE(json_data, \'$.type\') AS type,
